74ff3cdda3ebee057d6bcf48fb386f93381b5b38
Daryll Santos <daryll.santos@gmail.com>
2017-05-14 20:35:18 +0800

    Add way to start editing last post immediately.

--- blog_post.rb
+++ blog_post.rb
@@ -3,12 +3,27 @@ require 'tzinfo'
 require './new_blog_post_generator'
 
 class BlogPost < Thor
+  PROGRAMMING_BLOG_POSTS_DIR = './_posts/programming/'
+
+  map 'n' => :new
+  map 'o' => :open
+
   desc 'new', 'create new blog post'
 
   def new
     text_to_insert = NewBlogPostGenerator.new.generate_string
-    file_path = "./_posts/programming/#{DateTime.now.strftime("%Y-%m-%d")}-til.md"
+    file_path = "#{PROGRAMMING_BLOG_POSTS_DIR}#{DateTime.now.strftime("%Y-%m-%d")}-til.md"
     new_file_name = File.open(file_path, 'w').puts(text_to_insert)
     puts "Created file #{file_path}"
+  end
+
+  desc 'open', 'opens last blog post'
+
+  def open
+    last_file_created_path = Dir
+      .glob(File.join(PROGRAMMING_BLOG_POSTS_DIR, '*.*'))
+      .max { |a,b| File.ctime(a) <=> File.ctime(b) }
+
+    exec("vim -O2 #{last_file_created_path}")
   end
 end
